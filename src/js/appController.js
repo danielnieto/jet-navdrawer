ee3c67a0cdf973efb7361db246c9b78d343b5270
Daniel Nieto <danielnieto89@gmail.com>
2017-02-06 14:26:39 -0600

    add "responsive" module
    meant for responsive design testing

--- src/js/appController.js
+++ src/js/appController.js
@@ -20,7 +20,8 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarray
        // Router setup
        self.router = oj.Router.rootInstance;
        self.router.configure({
-         'dashboard': {label: 'Dashboard', isDefault: true},
+         'responsive': {label: 'Responsive', isDefault: true},
+         'dashboard': {label: 'Dashboard'},
          'incidents': {label: 'Incidents'},
          'customers': {label: 'Customers'},
          'about': {label: 'About'}
@@ -29,6 +30,8 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarray
 
       // Navigation setup
       var navData = [
+      {name: 'Responsive', id: 'responsive',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-catalog-icon-24'},
       {name: 'Dashboard', id: 'dashboard',
        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'},
       {name: 'Incidents', id: 'incidents',
